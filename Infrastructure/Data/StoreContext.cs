03d380ea71b322998c4a154461072226995f4c6e
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-08 13:12:06 +0300

    Configure Entities

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,5 +10,10 @@ using System.Threading.Tasks;
 namespace Infrastructure.Data {
     public class StoreContext(DbContextOptions options) : DbContext(options) {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
     }
 }
