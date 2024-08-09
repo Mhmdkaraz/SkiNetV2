1463051821d0727a9e872fef97c03d15b670882d
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-09 23:29:56 +0300

    Adding Projection to spec

--- Core/Specification/BaseSpecification.cs
+++ Core/Specification/BaseSpecification.cs
@@ -16,11 +16,25 @@ namespace Core.Specification {
 
         public Expression<Func<T, object>>? OrderByDesc { get; private set; }
 
+        public bool IsDistinct {  get; private set; }
+
         protected void AddOrderBy(Expression<Func<T, object>> orderByExpression) {
             OrderBy = orderByExpression; 
         }
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression) {
             OrderByDesc = orderByDescExpression;
+        }
+        protected void ApplyDistinct() {
+            IsDistinct = true;
+        }
+    }
+    public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria)
+        : BaseSpecification<T>(criteria), ISpecification<T, TResult> {
+        protected BaseSpecification() : this(null) { }
+
+        public Expression<Func<T, TResult>>? Select { get; private set; }
+        protected void AddSelect(Expression<Func<T, TResult>> selectExpression) {
+            Select = selectExpression; 
         }
     }
 }
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Data {
     public class SpecificationEvaluator<T> where T : BaseEntity{
-        public static IQueryable<T> GetQuery(IQueryable<T> query,ISpecification<T> spec) {
+        public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec) {
 
             if (spec.Criteria != null) { 
                 query = query.Where(spec.Criteria);
@@ -19,7 +19,29 @@ namespace Infrastructure.Data {
             if (spec.OrderByDesc != null) { 
                 query = query.OrderByDescending(spec.OrderByDesc);
             }
+            if (spec.IsDistinct) {
+                query = query.Distinct();
+            }
             return query;
+        }
+        public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec) {
+            if (spec.Criteria != null) {
+                query = query.Where(spec.Criteria);
+            }
+            if (spec.OrderBy != null) {
+                query = query.OrderBy(spec.OrderBy);
+            }
+            if (spec.OrderByDesc != null) {
+                query = query.OrderByDescending(spec.OrderByDesc);
+            }
+            var selectQuery = query as IQueryable<TResult>;
+            if (spec.Select != null) { 
+                selectQuery = query.Select(spec.Select);
+            }
+            if (spec.IsDistinct) {
+                selectQuery = selectQuery?.Distinct();
+            }
+            return selectQuery ?? query.Cast<TResult>();
         }
     }
 }
