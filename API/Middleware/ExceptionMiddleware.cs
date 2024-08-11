c4e0de407561b618ac9aabb87355e61b14a0071b
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-11 14:28:38 +0300

    Added Error Controller - Added ExceptionMiddleware - Added CORS

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using API.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Middleware {
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware {
+        private readonly IHostEnvironment _env;
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(IHostEnvironment env, RequestDelegate next) {
+            _env = env;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) {
+            try {
+                await _next(context);
+            } catch (Exception ex) {
+                await HandleExceptionAsync(context, ex, _env);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env) {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var response = env.IsDevelopment()
+                ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal server error");
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+            var json = JsonSerializer.Serialize(response, options);
+            return context.Response.WriteAsync(json);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder) {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
