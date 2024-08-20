2f40d1c091c99ac9463d4359e704d3f913038780
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-20 12:09:26 +0300

    Updating HeaderComponent - Persisting Login

--- client/src/app/core/services/account.service.ts
+++ client/src/app/core/services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Address, User } from '../../shared/models/user';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -25,9 +26,12 @@ export class AccountService {
   getUserInfo() {
     return this.http
       .get<User>(this.baseUrl + 'account/user-info', { withCredentials: true })
-      .subscribe({
-        next: (user) => this.currentUser.set(user),
-      });
+      .pipe(
+        map((user) => {
+          this.currentUser.set(user);
+          return user;
+        })
+      );
   }
   logout() {
     return this.http.post(
