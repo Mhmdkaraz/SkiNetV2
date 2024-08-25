a2d427cb01915af5feaf229207ec74a9f06242bc
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-25 15:19:49 +0300

    Implementing the payment intent

--- Core/Interfaces/IPaymentService.cs
+++ Core/Interfaces/IPaymentService.cs
@@ -2,6 +2,6 @@
 
 namespace Core.Interfaces {
     public interface IPaymentService {
-        Task<ShoppingCart> CreateOrUpdatePaymentIntent(string cartId);
+        Task<ShoppingCart?> CreateOrUpdatePaymentIntent(string cartId);
     }
 }
--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -1,10 +1,51 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 
 namespace Infrastructure.Services {
-    public class PaymentService : IPaymentService {
-        public Task<ShoppingCart> CreateOrUpdatePaymentIntent(string cartId) {
-            throw new NotImplementedException();
+    public class PaymentService(IConfiguration config, ICartService cartService,
+        IGenericRepository<Core.Entities.Product> productRepo,
+        IGenericRepository<DeliveryMethod> dmRepo) : IPaymentService {
+        //validate cart, delivery information then use this information to create an intent 
+        public async Task<ShoppingCart?> CreateOrUpdatePaymentIntent(string cartId) {
+            StripeConfiguration.ApiKey = config["StripeSettings:SecretKey"];
+            var cart = await cartService.GetCartAsync(cartId);
+            if (cart == null) {
+                return null;
+            }
+            var shippingPrice = 0m;
+            if (cart.DeliveryMethodId.HasValue) {
+                var deliveryMethod = await dmRepo.GetByIdAsync(cart.DeliveryMethodId.Value);
+                if (deliveryMethod == null) return null;
+                shippingPrice = deliveryMethod.Price;
+            }
+            foreach (var item in cart.Items) {
+                var productItem = await productRepo.GetByIdAsync(item.ProductId);
+                if (productItem == null) return null;
+                if (item.Price != productItem.Price) {
+                    item.Price = productItem.Price;
+                }
+            }
+            var service = new PaymentIntentService();
+            PaymentIntent? intent = null;
+            if (string.IsNullOrEmpty(cart.PaymentIntentId)) {
+                var options = new PaymentIntentCreateOptions {
+                    Amount = (long)cart.Items.Sum(x => x.Quantity * (x.Price * 100)) + (long)shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = ["cart"]
+                };
+                intent = await service.CreateAsync(options);
+                cart.PaymentIntentId = intent.Id;
+                cart.ClientSecret = intent.ClientSecret;
+            }else {
+                var options = new PaymentIntentUpdateOptions {
+                    Amount = (long)cart.Items.Sum(x => x.Quantity * (x.Price * 100)) + (long)shippingPrice * 100
+                };
+                intent = await service.UpdateAsync(cart.PaymentIntentId, options);
+            }
+            await cartService.SetCartAsync(cart);
+            return cart;
         }
     }
 }
