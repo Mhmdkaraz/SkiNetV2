46f98d8c1b17f1d18af3984c4ab7280c185e648e
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-25 13:43:25 +0300

    Setting up stripe

---
+++ Infrastructure/Services/PaymentService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Services {
+    public class PaymentService : IPaymentService {
+        public Task<ShoppingCart> CreateOrUpdatePaymentIntent(string cartId) {
+            throw new NotImplementedException();
+        }
+    }
+}
