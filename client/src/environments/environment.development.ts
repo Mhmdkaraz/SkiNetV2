3fe4fc372c6c7b760d0bda59a08875e0faa287ac
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-26 11:09:01 +0300

    Adding Client side Stripe

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
- production:false,
- apiUrl: 'https://localhost:5001/api/'
+  production: false,
+  apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51Prd8jF3r8u7Kurf4122VJih9aDAroihj5hIFz6m8JhlL31SeqIfVt5y82NYkZbiqOqIvIEj3L9mmUwU8FYr8IeE00KuQoKMgR',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51Prd8jF3r8u7Kurf4122VJih9aDAroihj5hIFz6m8JhlL31SeqIfVt5y82NYkZbiqOqIvIEj3L9mmUwU8FYr8IeE00KuQoKMgR',
 };
