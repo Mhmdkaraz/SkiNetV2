5db0aaedde6b4ea7a346dda4b3d65686eb2ba1bc
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-31 22:56:57 +0300

    Added Include for mapped properties

--- Core/Specification/OrderSpecification.cs
+++ Core/Specification/OrderSpecification.cs
@@ -3,8 +3,13 @@
 namespace Core.Specification {
     public class OrderSpecification : BaseSpecification<Order> {
         public OrderSpecification(string email) : base(x => x.BuyerEmail == email) {
+            AddInclude(x => x.OrderItems);
+            AddInclude(x => x.DeliveryMethod);
+            AddOrderByDescending(x=>x.OrderDate);
         }
         public OrderSpecification(string email, int id) : base(x => x.BuyerEmail == email && x.Id == id) {
+            AddInclude("OrderItems");   //OrderItems.XXX
+            AddInclude("DeliveryMethod");
         }
     }
 }
