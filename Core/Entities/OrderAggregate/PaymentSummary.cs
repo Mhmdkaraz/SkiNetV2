0dbaed97d8d791975f5732692663f3fb96103d48
Mhmdkaraz <mohamedkaraz22@gmail.com>
2024-08-31 14:57:35 +0300

    Added OrdersController - Added Order Creation - PaymentSummaryCorrection

--- Core/Entities/OrderAggregate/PaymentSummary.cs
+++ Core/Entities/OrderAggregate/PaymentSummary.cs
@@ -3,6 +3,6 @@
         public int Last4 { get; set; }
         public required string Brand { get; set; }
         public int ExpMonth { get; set; }
-        public int Year { get; set; }
+        public int ExpYear { get; set; }
     }
 }
